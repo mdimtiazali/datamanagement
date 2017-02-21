2eb99f400b01609c70acb3217401b3a037a6289a
Heiko Waldschmidt <wald@mueller-elektronik.de>
2017-02-21 14:28:28 +0100

    fixing defects #3372, #3120 - use xml for tablerows now TODO: - remove linear layout in product_mix_dialog_application_rate_table_row - add docu - remove duplicated code
    Change-Id: Ib879008ed3584ac5ee16a8d96e17f37e37053b85

--- datamanagement-ui/src/main/java/com/cnh/pf/android/data/management/productlibrary/views/ApplicationRateTableFactory.java
+++ datamanagement-ui/src/main/java/com/cnh/pf/android/data/management/productlibrary/views/ApplicationRateTableFactory.java
@@ -9,11 +9,7 @@
 package com.cnh.pf.android.data.management.productlibrary.views;
 
 import android.content.Context;
-import android.graphics.Typeface;
-import android.text.TextUtils;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
@@ -47,44 +43,25 @@ public class ApplicationRateTableFactory {
    public static TableRow createTableRowForProductMixDialog(Product product, Context context, TableLayout productMixTable, MeasurementSystem measurementSystemProductOther) {
       TableRow tableRow = null;
       if (product != null) {
-         tableRow = new TableRow(context);
+         LayoutInflater layoutInflater = LayoutInflater.from(context);
+         tableRow = (TableRow) layoutInflater.inflate(R.layout.product_mix_dialog_application_rate_table_row, null);
+
          ProductUnits unit = ProductHelperMethods.retrieveProductRateUnits(product, measurementSystemProductOther);
          int tableRowBackground = R.drawable.product_mix_dialog_application_rates_table_background_cell;
          if (productMixTable.getChildCount() - 1 % 2 == 1) {
             tableRowBackground = R.drawable.product_mix_dialog_application_rates_table_background_cell_gray;
          }
-         tableRow.setGravity(Gravity.CENTER);
 
-         LinearLayout productNameLayout = new LinearLayout(context);
+         LinearLayout productNameLayout = (LinearLayout) tableRow.findViewById(R.id.product_name_layout);
          productNameLayout.setBackgroundResource(tableRowBackground);
-         productNameLayout.setOrientation(LinearLayout.HORIZONTAL);
-         productNameLayout.setGravity(Gravity.START | Gravity.CENTER);
 
-         ImageView productTypeImage = new ImageView(context);
-         productTypeImage.setMaxWidth(context.getResources().getDimensionPixelOffset(R.dimen.product_mix_product_image_width_height));
-         productTypeImage.setMinimumWidth(context.getResources().getDimensionPixelOffset(R.dimen.product_mix_product_image_width_height));
-         productTypeImage.setMaxHeight(context.getResources().getDimensionPixelOffset(R.dimen.product_mix_product_image_width_height));
-         productTypeImage.setMinimumHeight(context.getResources().getDimensionPixelOffset(R.dimen.product_mix_product_image_width_height));
+         ImageView productTypeImage = (ImageView) tableRow.findViewById(R.id.product_name_image_view);
          productTypeImage.setBackgroundResource(getProductTypeImageId(product.getForm()));
-         productNameLayout.addView(productTypeImage, context.getResources().getDimensionPixelOffset(R.dimen.product_mix_product_image_width_height),
-               context.getResources().getDimensionPixelOffset(R.dimen.product_mix_product_image_width_height));
 
-         TextView productNameTextView = new TextView(context);
+         TextView productNameTextView = (TextView) tableRow.findViewById(R.id.product_name_text_view);
          productNameTextView.setText(product.getName());
-         productNameTextView.setTextSize(context.getResources().getDimensionPixelSize(R.dimen.product_mix_dialog_application_rate_name_text_size));
-         productNameTextView.setTextColor(context.getResources().getColor(R.color.defaultTextColor));
-         productNameTextView.setPadding(10, 0, 0, 0);
-         productNameTextView.setTypeface(null, Typeface.BOLD);
-         productNameLayout.addView(productNameTextView);
 
-         tableRow.addView(productNameLayout);
-
-         TextView applicationRate1TextView = new TextView(context);
-         applicationRate1TextView.setTextSize(context.getResources().getDimensionPixelSize(R.dimen.product_mix_dialog_application_rate_text_size));
-         applicationRate1TextView.setTextColor(context.getResources().getColor(R.color.defaultTextColor));
-         applicationRate1TextView.setTypeface(null, Typeface.BOLD);
-         applicationRate1TextView.setGravity(Gravity.START | Gravity.CENTER);
-         applicationRate1TextView.setEllipsize(TextUtils.TruncateAt.MIDDLE);
+         TextView applicationRate1TextView = (TextView) tableRow.findViewById(R.id.application_rate1_text_view);
          applicationRate1TextView.setBackgroundResource(tableRowBackground);
          if (unit != null) {
             applicationRate1TextView.setText(String.format("   %.2f %s", product.getDefaultRate() * unit.getMultiplyFactorFromBaseUnits(), unit.getName()));
@@ -92,14 +69,8 @@ public class ApplicationRateTableFactory {
          else {
             applicationRate1TextView.setText(String.format("   %.2f", product.getDefaultRate()));
          }
-         tableRow.addView(applicationRate1TextView, FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
 
-         TextView applicationRate2TextView = new TextView(context);
-         applicationRate2TextView.setTextSize(context.getResources().getDimensionPixelSize(R.dimen.product_mix_dialog_application_rate_text_size));
-         applicationRate2TextView.setTypeface(null, Typeface.BOLD);
-         applicationRate2TextView.setGravity(Gravity.START | Gravity.CENTER);
-         applicationRate2TextView.setEllipsize(TextUtils.TruncateAt.MIDDLE);
-         applicationRate2TextView.setTextColor(context.getResources().getColor(R.color.defaultTextColor));
+         TextView applicationRate2TextView = (TextView) tableRow.findViewById(R.id.application_rate2_text_view);
          applicationRate2TextView.setBackgroundResource(tableRowBackground);
          if (unit != null) {
             applicationRate2TextView.setText(String.format("   %.2f %s", product.getRate2() * unit.getMultiplyFactorFromBaseUnits(), unit.getName()));
@@ -107,56 +78,42 @@ public class ApplicationRateTableFactory {
          else {
             applicationRate2TextView.setText(String.format("   %.2f", product.getRate2()));
          }
-
-         tableRow.addView(applicationRate2TextView, FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
       }
       return tableRow;
    }
 
    /**
-    * create Tablerow with product data
+    * Create TableRow with product data
     * @param product current product to extract the data into the several cells
     * @return created TableRow
     */
    public static TableRow createTableRowForProductMixAdapter(Product product, Context context, MeasurementSystem volumeMeasurementSystem, MeasurementSystem massMeasurementSystem) {
-      TableRow tableRow = new TableRow(context);
+      TableRow tableRow = null;
       if (product != null) {
+         LayoutInflater layoutInflater = LayoutInflater.from(context);
+         tableRow = (TableRow) layoutInflater.inflate(R.layout.product_mix_application_rate_table_row, null);
+
          ProductUnits unit = ProductHelperMethods.retrieveProductRateUnits(product,
                ProductHelperMethods.getMeasurementSystemForProduct(product, volumeMeasurementSystem, massMeasurementSystem));
-         tableRow.setGravity(Gravity.CENTER);
-         TextView productNameTextView = new TextView(context);
-         productNameTextView.setText(" " + product.getName());
-         productNameTextView.setTextSize(context.getResources().getDimensionPixelSize(R.dimen.product_mix_overview_application_rate_text_size));
-         productNameTextView.setTextColor(context.getResources().getColor(R.color.defaultTextColor));
-         productNameTextView.setTypeface(null, Typeface.BOLD);
-         productNameTextView.setBackgroundResource(R.drawable.product_mix_dialog_application_rates_table_background_cell);
-         tableRow.addView(productNameTextView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
-         TextView applicationRate1TextView = new TextView(context);
-         applicationRate1TextView.setTextSize(context.getResources().getDimensionPixelSize(R.dimen.product_mix_overview_application_rate_text_size));
-         applicationRate1TextView.setTextColor(context.getResources().getColor(R.color.defaultTextColor));
-         applicationRate1TextView.setTypeface(null, Typeface.BOLD);
-         applicationRate1TextView.setBackgroundResource(R.drawable.product_mix_dialog_application_rates_table_background_cell);
+         TextView productNameTextView = (TextView) tableRow.findViewById(R.id.product_name_text_view);
+         productNameTextView.setText(" " + product.getName());
+
+         TextView applicationRate1TextView = (TextView) tableRow.findViewById(R.id.application_rate1_text_view);
          if (unit != null) {
             applicationRate1TextView.setText(String.format(" %.2f %s", product.getDefaultRate() * unit.getMultiplyFactorFromBaseUnits(), unit.getName()));
          }
          else {
             applicationRate1TextView.setText(String.format(" %.2f %s", product.getDefaultRate(), ""));
          }
-         tableRow.addView(applicationRate1TextView, FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
 
-         TextView applicationRate2TextView = new TextView(context);
-         applicationRate2TextView.setTextSize(context.getResources().getDimensionPixelSize(R.dimen.product_mix_overview_application_rate_text_size));
-         applicationRate2TextView.setTextColor(context.getResources().getColor(R.color.defaultTextColor));
-         applicationRate2TextView.setTypeface(null, Typeface.BOLD);
-         applicationRate2TextView.setBackgroundResource(R.drawable.product_mix_dialog_application_rates_table_background_cell);
+         TextView applicationRate2TextView = (TextView) tableRow.findViewById(R.id.application_rate2_text_view);;
          if (unit != null) {
             applicationRate2TextView.setText(String.format(" %.2f %s", product.getRate2() * unit.getMultiplyFactorFromBaseUnits(), unit.getName()));
          }
          else {
             applicationRate2TextView.setText(String.format(" %.2f %s", product.getRate2(), ""));
          }
-         tableRow.addView(applicationRate2TextView, FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
       }
       return tableRow;
    }
