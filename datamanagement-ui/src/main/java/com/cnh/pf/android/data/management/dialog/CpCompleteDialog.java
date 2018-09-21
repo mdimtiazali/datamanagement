e1efbcbfab2958a275ac6aa5e12904cf9a463599
lifeng <lianglifeng.us@gmail.com>
2018-09-21 16:12:06 -0500

    pfhmi-dev-defects-12203 - Data Management Tab:Copy + Paste -- Operation UI improvement pfhmi-dev-defects-14246 Display stucks on loading spinner after removing GFFT and Product pfhmi-dev-defects-14297 - Edit for Variety not available
    Change-Id: I8f234cbe9e1659fad2a37d4a0c6823400d02df2e

--- datamanagement-ui/src/main/java/com/cnh/pf/android/data/management/dialog/CpCompleteDialog.java
+++ datamanagement-ui/src/main/java/com/cnh/pf/android/data/management/dialog/CpCompleteDialog.java
@@ -9,6 +9,9 @@
 package com.cnh.pf.android.data.management.dialog;
 
 import android.content.Context;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 import android.view.View;
 import android.widget.TextView;
 import com.cnh.android.dialog.DialogView;
@@ -23,17 +26,35 @@ public class CpCompleteDialog extends DialogView {
         super(context);
         View view = View.inflate(context, R.layout.cp_complete_dialog, null);
         TextView notice = (TextView) view.findViewById(R.id.notice);
+        View v = view.findViewById(R.id.span);
+        if(names.size() > 3){
+            v.setVisibility(View.GONE);
+        }
+
         notice.setText(getResources().getString(R.string.cp_complete_content,field));
         SimpleTreeView treeView = (SimpleTreeView)view.findViewById(R.id.simpletreeview);
-        treeView.setItems(folderIconId, folderName,ids,names);
+        TextView singleItemView = (TextView)view.findViewById(R.id.singleitem);
+        if(names.size() == 1){
+            treeView.setVisibility(View.GONE);
+            ImageSpan imgSpan = new ImageSpan(context, ids.get(0));
+            String temp = "12  "+names.get(0);
+            SpannableString spannableString = new SpannableString(temp);
+            spannableString.setSpan(imgSpan, 0, 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            singleItemView.setText(spannableString);
+        }
+        else{
+            singleItemView.setVisibility(View.GONE);
+            treeView.setItems(folderIconId, folderName,ids,names);
+        }
+
         setTitle(getResources().getString(R.string.copy));
+        setContentPaddings(0,0,0,0);
         setBodyView(view);
         setFirstButtonText(getResources().getString(R.string.done));
         showFirstButton(true);
         showSecondButton(false);
         showThirdButton(false);
         setDialogWidth(getResources().getInteger(R.integer.copy_paste_dialog_width));
-        setBodyHeight(names.size() < getResources().getInteger(R.integer.copy_paste_dialog_high_threld) ? getResources().getInteger(R.integer.copy_paste_dialog_height_low)
-                :getResources().getInteger(R.integer.copy_paste_dialog_height_high));
+        setBodyHeight(getResources().getInteger(R.integer.copy_paste_dialog_height_high));
     }
 }
